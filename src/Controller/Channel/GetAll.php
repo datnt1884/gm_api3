9b8c3d19aeb5fd998a8a6b744c70f0c0cae8e2b9
laoxdat <laoxdat@gmail.com>
2020-05-09 21:05:36 +0700

    update

--- src/Controller/Channel/GetAll.php
+++ src/Controller/Channel/GetAll.php
@@ -17,11 +17,7 @@ final class GetAll extends Base
         $Channels = $this->getChannelService()->getAll((int) $deviceid[0]['login_data_id']);
         for ($i=0;$i<count($Channels);$i++)
         {
-            //   $epotime = strtotime($Channels[$i]['program_start']);
-            //    echo $epotime;
-            // exit;
             $Channels[$i]['channel_streams.token_url'] = "http://class.ttvmax.com/gen_token.php";
-            //$books[$i]['channel.stream_url'] = "http://";
         }
 
         return $this->jsonResponse($response, 'success', $Channels, 200);
